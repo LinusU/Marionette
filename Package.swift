30b6b5e9cee59a2dfbab6951669aff14e66255ec
Linus Unnebäck <linus@folkdatorn.se>
2018-12-11 15:59:28 +0000

    🎉 Use alpha of JSBridge instead of specific branch

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/artman/Signals", from: "6.0.0"),
-        .package(url: "https://github.com/LinusU/JSBridge", .branch("call-into-swift")),
+        .package(url: "https://github.com/LinusU/JSBridge", from: "1.0.0-alpha.10"),
         .package(url: "https://github.com/mxcl/PromiseKit", from: "6.0.0"),
     ],
     targets: [
